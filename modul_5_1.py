365acf6136951b27e27bfb60d0c46fa99377e7b1
ZOIA12 <varia2019@gmail.com>
2024-10-15 16:09:08 +0300

    Add files via upload

---
+++ modul_5_1.py
@@ -0,0 +1,30 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print(f'Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+
+house = House('ЖК Эльбрус',30)
+print(house.name, house.number_of_floors)
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+
+
+
+
+
+
+
